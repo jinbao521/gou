5710b44cde31d5299526ef3413600bfd522f3d9c
jinbao521 <415562591@qq.com>
2017-05-19 17:23:51 +0800

    永不言弃组 项目开始

---
+++ bb/public/javascripts/show.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	document.documentElement.style.fontSize = document.documentElement.clientWidth / 320 * 20 + 'px';
+	window.onresize = function() {
+		document.documentElement.style.fontSize = document.documentElement.clientWidth / 320 * 20 + 'px';
+	};
+	//rem计算
+}
